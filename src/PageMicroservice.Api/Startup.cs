728ff03cd8c5ffc1f78fc11384800eac592ae3a9
iGilga <pyua87@gmail.com>
2016-01-25 19:26:47 +0300

    Add connectstring

--- src/PageMicroservice.Api/Startup.cs
+++ src/PageMicroservice.Api/Startup.cs
@@ -3,9 +3,11 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Data.Entity;
 using Microsoft.Extensions.DependencyInjection;
 using Nancy.Owin;
 using PageMicroservice.Api.Configurations;
+using PageMicroservice.Data.Contexts;
 
 namespace PageMicroservice.Api
 {
@@ -15,6 +17,13 @@ namespace PageMicroservice.Api
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=PageMicroserviceDb;Trusted_Connection=True;";
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<PageContext>(options => options.UseSqlServer(connection));
+
             services.AddMvc();
 
             var builder= new ContainerBuilder();

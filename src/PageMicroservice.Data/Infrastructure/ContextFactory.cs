9016365ab27769f669df4c1569d90aef44837acb
iGilga <pyua87@gmail.com>
2016-01-27 09:45:48 +0300

    Autofac не уничтожает contextFactory, следовательно не вызывает dispose на уничтожение PageContext. Исключен dispose.

--- src/PageMicroservice.Data/Infrastructure/ContextFactory.cs
+++ src/PageMicroservice.Data/Infrastructure/ContextFactory.cs
@@ -3,23 +3,16 @@ using PageMicroservice.Data.Contexts;
 
 namespace PageMicroservice.Data.Infrastructure
 {
-    public interface IContextFactory: IDisposable
+    public interface IContextFactory
     {
         PageContext Get();
     }
 
     public class ContextFactory: IContextFactory
     {
-        private PageContext context;
-
         public PageContext Get()
         {
-            return context ?? (context = new PageContext());
-        }
-
-        public void Dispose()
-        {
-            context?.Dispose();
+            return new PageContext();
         }
     }
 }

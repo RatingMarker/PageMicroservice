a6eb0b50132df8aabfde1695486b225778c6af22
iGilga <pyua87@gmail.com>
2016-01-31 10:44:47 +0300

    Добавлен настройка запуска для ненси

---
+++ src/PageMicroservice.Api/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Nancy;
+using Nancy.Bootstrappers.Autofac;
+using PageMicroservice.Api.Configurations;
+
+namespace PageMicroservice.Api
+{
+    internal class Bootstrapper: AutofacNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(ILifetimeScope existingContainer)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<AutofacModule>();
+
+            builder.Update(existingContainer.ComponentRegistry);
+        }
+    }
+}

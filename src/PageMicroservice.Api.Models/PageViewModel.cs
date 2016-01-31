baaf8b8659375996246642f3b2564bc01e13e61f
iGilga <pyua87@gmail.com>
2016-01-31 10:50:16 +0300

    Дто

---
+++ src/PageMicroservice.Api.Models/PageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageMicroservice.Api.Models
+{
+    public class PageViewModel
+    {
+        public int PageId { get; set; }
+        public string Uri { get; set; }
+        public DateTime? FoundDate { get; set; }
+        public DateTime? LastScanDate { get; set; }
+        public int SiteId { get; set; }
+        public SiteViewModel Site { get; set; }
+    }
+}
---
+++ src/PageMicroservice.Api.Models/SiteViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PageMicroservice.Api.Models
+{
+    public class SiteViewModel
+    {
+        public int SiteId { get; set; }
+        public string Name { get; set; }
+    }
+}

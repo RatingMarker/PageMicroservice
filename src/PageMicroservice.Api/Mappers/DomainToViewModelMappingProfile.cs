0446068d8da93bfb5e3ac330c5b695329786a53c
iGilga <pyua87@gmail.com>
2016-01-31 10:46:00 +0300

    Конфиг для автомаппера

---
+++ src/PageMicroservice.Api/Mappers/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PageMicroservice.Api.Models;
+using PageMicroservice.Models;
+
+namespace PageMicroservice.Api.Mappers
+{
+    public class DomainToViewModelMappingProfile: Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Site, SiteViewModel>();
+            CreateMap<Page, PageViewModel>();
+        }
+    }
+}

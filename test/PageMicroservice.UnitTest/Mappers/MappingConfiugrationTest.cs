32646cfa67032c3afdcad0452324f74b93f58082
iGilga <pyua87@gmail.com>
2016-02-01 12:10:56 +0300

    Тест маппинга

---
+++ test/PageMicroservice.UnitTest/Mappers/MappingConfiugrationTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PageMicroservice.Api.Models;
+using PageMicroservice.Models;
+using Xunit;
+
+namespace PageMicroservice.UnitTest.Mappers
+{
+    public class MappingConfiugrationTest
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            //arrange
+            MapperConfiguration config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<Site, SiteViewModel>();
+                });
+
+            //act
+
+
+            //assert
+            config.AssertConfigurationIsValid();
+        }
+    }
+}

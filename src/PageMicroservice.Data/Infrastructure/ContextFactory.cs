b3b8d8657f286a92dbe015e39623a43f96b61c2b
iGilga <pyua87@gmail.com>
2016-02-01 12:23:23 +0300

    Добавлены конструкторы

--- src/PageMicroservice.Data/Infrastructure/ContextFactory.cs
+++ src/PageMicroservice.Data/Infrastructure/ContextFactory.cs
@@ -12,9 +12,32 @@ namespace PageMicroservice.Data.Infrastructure
 
     public class ContextFactory: IContextFactory
     {
+        private readonly DbContextOptions<PageContext> options; 
+
+        public ContextFactory()
+        {
+            const string connectString = @"Server=(localdb)\mssqllocaldb;Database=PageMicroserviceDb;Trusted_Connection=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<PageContext>();
+            optionsBuilder.UseSqlServer(connectString);
+            options = optionsBuilder.Options;
+        }
+
+        public ContextFactory(string connectString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PageContext>();
+            optionsBuilder.UseSqlServer(connectString);
+            options = optionsBuilder.Options;
+        }
+
+        public ContextFactory(DbContextOptions<PageContext> options)
+        {
+            this.options = options;
+        }
+
         public PageContext Get()
         {
-            return new PageContext();
+            return new PageContext(options);
         }
     }
 }

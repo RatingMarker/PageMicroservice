6029f48f0c60dfa02b1115c772b1a2ffe75a94ea
iGilga <pyua87@gmail.com>
2016-03-05 04:02:21 +0300

    Rename property

--- src/PageMicroservice.Api/Models/Page.cs
+++ src/PageMicroservice.Api/Models/Page.cs
@@ -5,7 +5,7 @@ namespace PageMicroservice.Api.Models
     public class Page
     {
         public int PageId { get; set; }
-        public string Uri { get; set; }
+        public string Url { get; set; }
         public DateTime? FoundDate { get; set; }
         public DateTime? LastScanDate { get; set; }
         public int SiteId { get; set; }

95d462a989e92a730e01403ec061a68485e92a03
iGilga <pyua87@gmail.com>
2016-03-05 04:01:37 +0300

    Rename property

--- src/PageMicroservice.Api/ViewModels/PageViewModel.cs
+++ src/PageMicroservice.Api/ViewModels/PageViewModel.cs
@@ -5,10 +5,9 @@ namespace PageMicroservice.Api.ViewModels
     public class PageViewModel
     {
         public int PageId { get; set; }
-        public string Uri { get; set; }
+        public string Url { get; set; }
         public DateTime? FoundDate { get; set; }
         public DateTime? LastScanDate { get; set; }
         public int SiteId { get; set; }
-        public SiteViewModel Site { get; set; }
     }
 }

2995fadf73c78a9ce1699b4076d82bec44c4c1d2
iGilga <pyua87@gmail.com>
2016-03-05 03:55:13 +0300

    Remove comments and add nlog console

--- src/PageMicroservice.Api/Startup.cs
+++ src/PageMicroservice.Api/Startup.cs
@@ -2,22 +2,18 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nancy.Owin;
+using NLog.Framework.Logging;
 
 namespace PageMicroservice.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            //            var connection = @"Server=(localdb)\mssqllocaldb;Database=PageMicroserviceDb;Trusted_Connection=True;";
-            //            services.AddEntityFramework()
-            //                    .AddSqlServer()
-            //                    .AddDbContext<PageContext>(options => options.UseSqlServer(connection));
-
             var builder = new ContainerBuilder();
 
             builder.Populate(services);
@@ -27,9 +23,10 @@ namespace PageMicroservice.Api
             return container.Resolve<IServiceProvider>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("nlog.config");
             app.UseOwin(owin => owin.UseNancy(options => options.Bootstrapper = new Bootstrapper()));
         }
     }
